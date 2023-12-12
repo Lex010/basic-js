53926bcb199c35dbfa040eb87c314f9745aa81f3
Lex Mel <lex07672@gmail.com>
2023-12-12 17:55:28 +0200

    feat: do carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,10 +17,16 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-function dateSample(/* sampleActivity */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function dateSample(sampleActivity) {
+  const numericActivity = parseFloat(sampleActivity);
+  if (isNaN(numericActivity) || typeof sampleActivity !== 'string' || numericActivity <= 0 || numericActivity > MODERN_ACTIVITY) {
+    return false;
+  }
+  const k = 0.693 / HALF_LIFE_PERIOD;
+  const age = Math.ceil(Math.log(MODERN_ACTIVITY / numericActivity) / k);
+  return age;
 }
+
 
 module.exports = {
   dateSample
