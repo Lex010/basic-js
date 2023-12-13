ae391d4f53ad699e7db2d4306a415a3f892c1c5d
Lex Mel <lex07672@gmail.com>
2023-12-13 16:05:06 +0200

    feat: last 2 task with 1 no complete point

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -20,13 +20,87 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(isDirect = true) {
+    this.isDirect = isDirect;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+
+  encrypt(message, key) {
+    this.validateInput(message, key);
+
+    message = message.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+
+    for (let i = 0, j = 0; i < message.length; i++) {
+      const currentChar = message[i];
+      if (this.isUppercaseLetter(currentChar)) {
+        const messageCharCode = currentChar.charCodeAt(0);
+        const keyCharCode = key[j % key.length].charCodeAt(0);
+
+        const encryptedCharCode = this.calculateVigenereCipher(messageCharCode, keyCharCode);
+        result += String.fromCharCode(encryptedCharCode);
+
+        j++;
+      } else {
+        result += currentChar;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  decrypt(encryptedMessage, key) {
+    this.validateInput(encryptedMessage, key);
+
+    encryptedMessage = encryptedMessage.toUpperCase();
+    key = key.toUpperCase();
+
+    let result = '';
+
+    for (let i = 0, j = 0; i < encryptedMessage.length; i++) {
+      const currentChar = encryptedMessage[i];
+      if (this.isUppercaseLetter(currentChar)) {
+        const encryptedCharCode = currentChar.charCodeAt(0);
+        const keyCharCode = key[j % key.length].charCodeAt(0);
+
+        const decryptedCharCode = this.calculateVigenereDecipher(encryptedCharCode, keyCharCode);
+        result += String.fromCharCode(decryptedCharCode);
+
+        j++;
+      } else {
+        result += currentChar;
+      }
+    }
+
+    return this.isDirect ? result : result.split('').reverse().join('');
+  }
+
+  validateInput(message, key) {
+    if ((!message && message !== '') || (!key && key !== '')) {
+      throw new Error('Both message and key are required.');
+    }
+
+    if (typeof message !== 'string' || typeof key !== 'string') {
+      throw new Error('Both message and key should be strings.');
+    }
+
+    if (!message.trim() || !key.trim()) {
+      throw new Error('Both message and key should not be empty.');
+    }
+  }
+
+  isUppercaseLetter(char) {
+    return /^[A-Z]$/.test(char);
+  }
+
+  calculateVigenereCipher(messageCharCode, keyCharCode) {
+    return ((messageCharCode + keyCharCode) % 26) + 'A'.charCodeAt(0);
+  }
+
+  calculateVigenereDecipher(encryptedCharCode, keyCharCode) {
+    const result = (encryptedCharCode - keyCharCode + 26) % 26;
+    return result + 'A'.charCodeAt(0);
   }
 }
 
