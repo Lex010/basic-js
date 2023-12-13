22c3668c2bb40e899d71143a19d9b28b4191e1da
Lex Mel <lex07672@gmail.com>
2023-12-13 15:16:50 +0200

    feat: sum-digits and transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,9 +13,43 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!");
+  }
+  const result = [];
+  // Мы будем использовать этот флаг, чтобы определить, следует ли игнорировать следующий элемент.
+  let discardNext = false;
+  for (let i = 0; i < arr.length; i++) {
+    const current = arr[i];
+    if (discardNext) {
+      discardNext = false;
+      continue; // Пропускаем текущий элемент
+    }
+    switch (current) {
+      case '--discard-next':
+        discardNext = true;
+        break;
+      case '--discard-prev':
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          result.pop();
+        }
+        break;
+      case '--double-next':
+        if (i < arr.length - 1) {
+          result.push(arr[i + 1]);
+        }
+        break;
+      case '--double-prev':
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          result.push(result[result.length - 1]);
+        }
+        break;
+      default:
+        result.push(current);
+    }
+  }
+  return result;
 }
 
 module.exports = {
